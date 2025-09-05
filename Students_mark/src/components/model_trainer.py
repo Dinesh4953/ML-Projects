88407a1a1d3d545ec6737f22af00d9a522976aa7
Dinesh4953 <dinu08642@gmail.com>
2025-09-05 09:33:19 +0530

    Restructured repo: Students_mark inside ML Projects

--- Students_mark/src/components/model_trainer.py
+++ Students_mark/src/components/model_trainer.py
@@ -38,18 +38,56 @@ class ModelTrainer:
                 test_array[:, -1],
             )
             models = {
-                "Linear Regression": LinearRegression(),
-    "Lasso": Lasso(),
-    "Ridge": Ridge(),
-    "K-Neighbors Regressor": KNeighborsRegressor(),
+                "Linear Regressor": LinearRegression(),
     "Decision Tree": DecisionTreeRegressor(),
     "Random Forest Regressor": RandomForestRegressor(),
     "XGBRegressor": XGBRegressor(), 
     "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-    "AdaBoost Regressor": AdaBoostRegressor()
+    "AdaBoost Regressor": AdaBoostRegressor(),
+    "Gradient Boosting Regressor" :  GradientBoostingRegressor()
+            }
+            params = {
+                 "Linear Regressor":{},
+                "Decision Tree" : {
+                    "criterion" : ['squared_error', 'absolute_error', 'friedman_mse', 'poisson'],
+                    "splitter" : ['best','random'],
+                    
+                },
+                "Random Forest Regressor" : {
+                    "criterion" : ["absolute_error","squared_error","friedman_mse", "poisson"],
+                    'n_estimators' : [8, 16, 32, 64, 128, 256]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                 
+                    "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                    "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                    "Gradient Boosting Regressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                 
+                   
+                    
+                   
+                
             }
             
-            model_report:dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test,y_test=y_test,  models=models)
+            model_report:dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test,y_test=y_test,  models=models, params=params)
             
             
             best_model_score = max(sorted(model_report.values()))
@@ -72,4 +110,6 @@ class ModelTrainer:
             
         except Exception as e:
             raise CustomException (e, sys)
+
+
 
